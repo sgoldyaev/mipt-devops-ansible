8b8b9c795323f5c84ddd56363c2c9b62e25c4304
Sergey Goldyaev <sergey.goldyaev@approvalmax.com>
2021-02-16 12:18:27 +0300

    add clean up

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,12 @@
-@rem script 1
+@rem script 1 "find server uptime"
 ansible localhost -a "uptime"
 
-@rem script 2 "run uptime"
-ansible-playbook -i hosts playbook0.yaml
+@rem script 2 "find server uptime via playbook"
+ansible-playbook -i hosts playbook0.yaml -v
 
-@rem script 3 "install apache"
-ansible-playbook -i hosts playbook1.yaml -b
+@rem script 3 "install apache via playbook"
+ansible-playbook -i hosts playbook1.yaml -b -v
+
+@rem script 4 "remove apache2 manually"
+sudo apt-get remove apache2-common
+sudo apt-get --purge remove apache2
